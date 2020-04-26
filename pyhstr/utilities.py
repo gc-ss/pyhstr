942a627929e042cc386762e556b82aa1e3a74c8f
adder46 <dedmauz69@gmail.com>
2020-04-26 19:00:30 +0200

    Minor tweaks

--- pyhstr/utilities.py
+++ pyhstr/utilities.py
@@ -8,32 +8,37 @@ class EntryCounter:
         self.value = 0
         self.app = app
 
-    def inc(self, page_size):
+    def inc(self):
+        page_size = self.app.user_interface.get_page_size(self.app.page.value)
         self.value += 1
         self.value = self.value % page_size
         if self.value == 0:
-            self.app.page.inc(self.app.user_interface.get_number_of_pages())
+            self.app.page.inc()
 
-    def dec(self, page_size):
+    def dec(self):
+        page_size = self.app.user_interface.get_page_size(self.app.page.value)
         self.value -= 1
         self.value = self.value % page_size
         # in both places, we are subtracting -1
         # because indexing starts from zero
         if self.value == (page_size - 1):
-            self.app.page.dec(self.app.user_interface.get_number_of_pages())
+            self.app.page.dec()
             self.value = self.app.user_interface.get_page_size(self.app.page.value) - 1
 
 class PageCounter:
-    def __init__(self):
+    def __init__(self, app):
         self.value = 1
+        self.app = app
 
-    def inc(self, total_pages):
+    def inc(self):
+        total_pages = self.app.user_interface.get_number_of_pages()
         if self.value == total_pages:
             self.value = 1
         else:
             self.value += 1
 
-    def dec(self, total_pages):
+    def dec(self):
+        total_pages = self.app.user_interface.get_number_of_pages()
         if self.value == 1:
             self.value = total_pages
         else:
